275987cc9983844e97b44c2584999cc59d8dac5f
Bibek08 <98956587+Bibek08@users.noreply.github.com>
2023-08-12 11:41:58 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new typed("#typing", {
+  strings: ["Frontend Developer", "YouTuber", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+});
